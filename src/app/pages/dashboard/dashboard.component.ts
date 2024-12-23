4130943c375f078d2d65de3e0e9b808cc4870c9b
Dmytro <dimka670020040@gmail.com>
2024-12-23 14:36:37 +0200

    header + sidebar

--- src/app/pages/dashboard/dashboard.component.ts
+++ src/app/pages/dashboard/dashboard.component.ts
@@ -1,11 +1,67 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
+import { HeaderComponent } from '../../shared/components/header/header.component';
+import { MatSidenav } from '@angular/material/sidenav';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatListModule } from '@angular/material/list';
+import { RouterOutlet } from '@angular/router';
+import { BreakpointObserver } from '@angular/cdk/layout';
+import { CommonModule, NgStyle } from '@angular/common';
+import { MatIconModule } from '@angular/material/icon';
 
 @Component({
   selector: 'app-dashboard',
-  imports: [],
+  imports: [
+    HeaderComponent,
+    MatSidenavModule,
+    MatListModule,
+    RouterOutlet,
+    CommonModule,
+    MatIconModule,
+  ],
   templateUrl: './dashboard.component.html',
-  styleUrl: './dashboard.component.scss'
+  styleUrl: './dashboard.component.scss',
+  host: {
+    class: 'dashboard',
+  },
 })
 export class DashboardComponent {
+  @ViewChild(MatSidenav)
+  sidenav!: MatSidenav;
+  isMobile = true;
+  isCollapsed = true;
+  isOpened = false;
 
+  constructor(private observer: BreakpointObserver) {}
+
+  ngOnInit() {
+    this.observer.observe(['(max-width: 800px)']).subscribe((screenSize) => {
+      if (screenSize.matches) {
+        this.isMobile = true;
+      } else {
+        this.isMobile = false;
+      }
+    });
+  }
+
+  // toggleMenu() {
+  //   if (this.isMobile) {
+  //     this.sidenav.toggle();
+  //     this.isCollapsed = false; // On mobile, the menu can never be collapsed
+  //   } else {
+  //     this.sidenav.open(); // On desktop/tablet, the menu can never be fully closed
+  //     this.isCollapsed = !this.isCollapsed;
+  //   }
+  // }
+
+  toggleMenu() {
+    this.sidenav.toggle();
+    this.isOpened = !this.isOpened;
+    if (this.isMobile) {
+      this.sidenav.toggle();
+      this.isCollapsed = false; // On mobile, the menu can never be collapsed
+    } else {
+      this.sidenav.open(); // On desktop/tablet, the menu can never be fully closed
+      this.isCollapsed = !this.isCollapsed;
+    }
+  }
 }
