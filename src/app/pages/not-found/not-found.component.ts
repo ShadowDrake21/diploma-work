7b01e49cc13a92210488f772fe06f462ddedff39
Dmytro <dimka670020040@gmail.com>
2025-01-04 11:07:00 +0200

    not-found page

--- src/app/pages/not-found/not-found.component.ts
+++ src/app/pages/not-found/not-found.component.ts
@@ -1,11 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { AuthService } from '../../core/authentication/auth.service';
+import { Router, RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-not-found',
   imports: [],
   templateUrl: './not-found.component.html',
-  styleUrl: './not-found.component.scss'
+  styleUrl: './not-found.component.scss',
 })
 export class NotFoundComponent {
+  private authService = inject(AuthService);
+  private router = inject(Router);
 
+  onGoBack() {
+    if (this.authService.isAuthenticated) {
+      this.router.navigate(['/']);
+    } else {
+      this.router.navigate(['/authentication']);
+    }
+  }
 }
