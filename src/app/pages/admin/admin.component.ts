34bf52eac59e22f8c6d47954c3e30924c3994159
Dmytro <dimka670020040@gmail.com>
2025-01-19 21:23:03 +0200

    dashboard: basic markup (without styling)

--- src/app/pages/admin/admin.component.ts
+++ src/app/pages/admin/admin.component.ts
@@ -1,11 +1,10 @@
 import { Component } from '@angular/core';
+import { RouterLink, RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-admin',
-  imports: [],
+  imports: [RouterOutlet],
   templateUrl: './admin.component.html',
-  styleUrl: './admin.component.scss'
+  styleUrl: './admin.component.scss',
 })
-export class AdminComponent {
-
-}
+export class AdminComponent {}
