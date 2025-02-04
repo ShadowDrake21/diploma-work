4936c2798c97246d441719ba4656a884c8e14986
Dmytro <dimka670020040@gmail.com>
2025-02-04 22:09:15 +0200

    user entity: part 1

---
+++ backend/src/main/java/com/backend/app/model/Role.java
@@ -0,0 +1,5 @@
+package com.backend.app.model;
+
+public enum Role {
+	USER, ADMIN
+}
--- backend/src/test/java/com/backend/app/BackendApplicationTests.java
+++ backend/src/test/java/com/backend/app/BackendApplicationTests.java
@@ -1,4 +1,4 @@
-package com.backend.demo;
+package com.backend.app;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
