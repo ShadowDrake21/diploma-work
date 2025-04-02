f6c07043969c03fc2a4ddb73fdf7926a8ee3020b
Dmytro <dimka670020040@gmail.com>
2025-04-02 14:00:16 +0300

    filters front end

--- src/app/core/services/project.service.ts
+++ src/app/core/services/project.service.ts
@@ -1,8 +1,30 @@
-import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { HttpClient, HttpHeaders, HttpParams } from '@angular/common/http';
 import { inject, Injectable } from '@angular/core';
 import { BASE_URL } from '@core/constants/default-variables';
 import { Project } from '@shared/types/project.types';
-import { catchError, Observable, tap } from 'rxjs';
+import { catchError, Observable, tap, throwError } from 'rxjs';
+
+interface ProjectFilters {
+  search?: string;
+  types?: string[];
+  tags?: string[];
+  dateRange?: {
+    start?: Date;
+    end?: Date;
+  };
+  assigned?: boolean;
+  inProgress?: boolean;
+  completed?: boolean;
+  progressMin?: number;
+  progressMax?: number;
+  publicationSource?: string;
+  doiIsbn?: string;
+  minBudget?: number;
+  maxBudget?: number;
+  fundingSource?: string;
+  registrationNumber?: string;
+  issuingAuthority?: string;
+}
 
 @Injectable({
   providedIn: 'root',
@@ -12,11 +34,15 @@ export class ProjectService {
 
   private apiUrl = BASE_URL + 'projects';
 
+  private get headers(): HttpHeaders {
+    return new HttpHeaders({
+      Authorization: `Bearer ${localStorage.getItem('authToken')}`,
+    });
+  }
+
   getAllProjects(): Observable<Project[]> {
     return this.http.get<Project[]>(`${this.apiUrl}`, {
-      headers: new HttpHeaders({
-        Authorization: `Bearer ${localStorage.getItem('authToken')}`,
-      }),
+      headers: this.headers,
     });
   }
 
@@ -24,9 +50,7 @@ export class ProjectService {
     console.log('getProjectById', `${this.apiUrl}/${id}`);
     return this.http
       .get<Project>(`${this.apiUrl}/${id}`, {
-        headers: new HttpHeaders({
-          Authorization: `Bearer ${localStorage.getItem('authToken')}`,
-        }),
+        headers: this.headers,
       })
       .pipe(
         tap((response) => console.log('Project response:', response)),
@@ -43,44 +67,85 @@ export class ProjectService {
       `${this.apiUrl}/${projectId}/publication`
     );
     return this.http.get(`${this.apiUrl}/${projectId}/publication`, {
-      headers: new HttpHeaders({
-        Authorization: `Bearer ${localStorage.getItem('authToken')}`,
-      }),
+      headers: this.headers,
     });
   }
 
   getPatentByProjectId(projectId: string): Observable<any> {
     return this.http.get(`${this.apiUrl}/${projectId}/patent`, {
-      headers: new HttpHeaders({
-        Authorization: `Bearer ${localStorage.getItem('authToken')}`,
-      }),
+      headers: this.headers,
     });
   }
 
   getResearchByProjectId(projectId: string): Observable<any> {
     return this.http.get(`${this.apiUrl}/${projectId}/research`, {
-      headers: new HttpHeaders({
-        Authorization: `Bearer ${localStorage.getItem('authToken')}`,
-      }),
+      headers: this.headers,
     });
   }
 
   createProject(project: any): Observable<any> {
     console.log('createProject', project);
     return this.http.post(`${this.apiUrl}`, project, {
-      headers: new HttpHeaders({
-        Authorization: `Bearer ${localStorage.getItem('authToken')}`,
-      }),
+      headers: this.headers,
     });
   }
 
   updateProject(id: string, project: any): Observable<any> {
     console.log('updateProject', project);
     return this.http.put(`${this.apiUrl}/${id}`, project, {
-      headers: new HttpHeaders({
-        Authorization: `Bearer ${localStorage.getItem('authToken')}`,
-      }),
+      headers: this.headers,
     });
+  }
+
+  searchProjects(filters: any): Observable<Project[]> {
+    let params = new HttpParams();
+
+    if (filters.search) params = params.set('search', filters.search);
+
+    if (filters.types.length)
+      params = params.set('types', filters.types.join(','));
+    if (filters.tags.length)
+      params = params.set('tags', filters.tags.join(','));
+    if (filters.dateRange.start)
+      params = params.set('startDate', filters.dateRange.start.toISOString());
+    if (filters.dateRange.end)
+      params = params.set('endDate', filters.dateRange.end.toISOString());
+
+    const statuses = [];
+
+    if (filters.assigned) statuses.push('assigned');
+    if (filters.isProgress) statuses.push('in_progress');
+    if (filters.completed) statuses.push('completed');
+    if (statuses.length) params = params.set('statuses', statuses.join(','));
+
+    params = params.set('progressMin', filters.progressMin.toString());
+    params = params.set('progressMax', filters.progressMax.toString());
+
+    if (filters.publicationSource)
+      params = params.set('publicationSource', filters.publicationSource);
+    if (filters.doiIsbn) params = params.set('doiIsbn', filters.doiIsbn);
+    if (filters.minBudget) params = params.set('minBudget', filters.minBudget);
+    if (filters.maxBudget) params = params.set('maxBudget', filters.maxBudget);
+    if (filters.fundingSource)
+      params = params.set('fundingSource', filters.fundingSource);
+    if (filters.registrationNumber)
+      params = params.set('registrationNumber', filters.registrationNumber);
+    if (filters.issuingAuthority)
+      params = params.set('issuingAuthority', filters.issuingAuthority);
+
+    return this.http
+      .get<Project[]>(`${this.apiUrl}/search`, {
+        params,
+        headers: this.headers,
+      })
+      .pipe(catchError(this.handleError));
+  }
+
+  private handleError(error: any): Observable<never> {
+    console.error('An error occurred:', error);
+    return throwError(
+      () => new Error('Something went wrong; please try again later.')
+    );
   }
 
   deleteProject(id: string): Observable<any> {
