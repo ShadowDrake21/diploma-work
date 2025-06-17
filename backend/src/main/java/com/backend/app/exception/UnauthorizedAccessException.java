c4319d0feec0716ea8c5337b71fc534866e593c3
Dmytro <dimka670020040@gmail.com>
2025-06-17 20:29:36 +0300

    s3 and other stuff

--- backend/src/main/java/com/backend/app/exception/UnauthorizedAccessException.java
+++ backend/src/main/java/com/backend/app/exception/UnauthorizedAccessException.java
@@ -2,7 +2,7 @@ package com.backend.app.exception;
 
 public class UnauthorizedAccessException extends RuntimeException {
 	private static final long serialVersionUID = 1L;
-
+	
 	public UnauthorizedAccessException(String message) {
 		super(message);
 	}
